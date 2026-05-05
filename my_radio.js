e2fb658935fc43df066143172b6340deb6ed137c
burlaicent-glitch <burlaicent@gmail.com>
2026-05-05 19:41:25 +0200

    Update my_radio.js

--- my_radio.js
+++ my_radio.js
@@ -1,46 +1,31 @@
 (function () {
     'use strict';
 
-    function RadioPlugin(api) {
-        this.create = function () {
-            // Список станцій (можна додавати свої посилання)
+    Lampa.Plugins.add('radio_plugin', function (api) {
+        function show() {
+            var scroll = Lampa.Template.get('items_list', {title: 'Радіо'});
             var stations = [
-                { title: 'Hit FM', url: 'https://online.hitfm.ua/HitFM', img: 'https://static.hitfm.ua/logo.png' },
-                { title: 'Radio ROKS', url: 'https://online.radioroks.ua/RadioROKS', img: 'https://static.radioroks.ua/logo.png' },
-                { title: 'Kiss FM', url: 'https://online.kissfm.ua/KissFM', img: 'https://static.kissfm.ua/logo.png' },
-                { title: 'Bayraktar', url: 'https://online.radiobayraktar.com.ua/RadioBayraktar', img: 'https://static.radiobayraktar.com.ua/logo.png' }
+                {title: 'Hit FM', url: 'https://online.hitfm.ua/HitFM'},
+                {title: 'Radio ROKS', url: 'https://online.radioroks.ua/RadioROKS'},
+                {title: 'Kiss FM', url: 'https://online.kissfm.ua/KissFM'}
             ];
 
-            // Створення головного екрану плагіна
-            var view = Lampa.Template.get('items_list', { title: 'Українське Радіо' });
-            
-            stations.forEach(function (station) {
-                var item = Lampa.Template.get('card', {
-                    title: station.title,
-                    card_category: 'Радіо'
-                });
-
-                item.find('.card__img').attr('src', station.img);
-                
+            stations.forEach(function (st) {
+                var item = Lampa.Template.get('card', {title: st.title, card_category: 'UA'});
                 item.on('hover:enter', function () {
-                    Lampa.Player.play({
-                        url: station.url,
-                        title: station.title
-                    });
+                    Lampa.Player.play({url: st.url, title: st.title});
                 });
-
-                view.find('.items-line').append(item);
+                scroll.find('.items-line').append(item);
             });
 
             Lampa.Select.show({
-                container: view,
-                onBack: function () {
-                    Lampa.Controller.toggle('main');
-                }
+                container: scroll,
+                onBack: function () { Lampa.Controller.toggle('main'); }
             });
-        };
-    }
+        }
 
-    // Реєстрація плагіна в меню
-    Lampa.Plugins.add('radio_ua', RadioPlugin);
+        var menu_item = $('<div class="menu__item selector"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 18v-6a9 9 0 0 1 18 0v6"></path><path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3z"></path><path d="M3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z"></path></svg></div><div class="menu__text">Радіо</div></div>');
+        menu_item.on('hover:enter', show);
+        $('.menu .menu__list').append(menu_item);
+    });
 })();
