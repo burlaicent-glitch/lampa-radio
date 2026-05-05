c8960e635f01da427328e75848e76e491c42d304
burlaicent-glitch <burlaicent@gmail.com>
2026-05-05 17:28:32 +0200

    Update my_radio.js

--- my_radio.js
+++ my_radio.js
@@ -1,39 +1,17 @@
 (function () {
     'use strict';
 
-    Lampa.Plugins.add('radio_ua_custom', function (api) {
-        var storage_key = 'lampa_my_radio_list';
-
-        function showRadio() {
-            var stations = JSON.parse(localStorage.getItem(storage_key) || '[]');
-            if (stations.length === 0) {
-                stations = [
-                    {title: 'Hit FM', url: 'https://online.hitfm.ua/HitFM'},
-                    {title: 'Radio ROKS', url: 'https://online.radioroks.ua/RadioROKS'},
-                    {title: 'Kiss FM', url: 'https://online.kissfm.ua/KissFM'}
-                ];
-            }
-
-            var scroll = Lampa.Template.get('items_list', {title: 'Моє Радіо'});
-            
-            var add_btn = Lampa.Template.get('card', {title: 'Додати станцію', card_category: 'Налаштування'});
-            add_btn.on('hover:enter', function () {
-                Lampa.Input.edit({title: 'Назва радіо'}, function (name) {
-                    if (name) {
-                        Lampa.Input.edit({title: 'URL посилання (mp3)', value: 'http://'}, function (url) {
-                            if (url) {
-                                stations.push({title: name, url: url});
-                                localStorage.setItem(storage_key, JSON.stringify(stations));
-                                Lampa.Noty.show('Збережено! Перезайдіть у радіо');
-                            }
-                        });
-                    }
-                });
-            });
-            scroll.find('.items-line').append(add_btn);
+    Lampa.Plugins.add('radio_plugin', function (api) {
+        function show() {
+            var scroll = Lampa.Template.get('items_list', {title: 'Радіо'});
+            var stations = [
+                {title: 'Hit FM', url: 'https://online.hitfm.ua/HitFM'},
+                {title: 'Radio ROKS', url: 'https://online.radioroks.ua/RadioROKS'},
+                {title: 'Kiss FM', url: 'https://online.kissfm.ua/KissFM'}
+            ];
 
             stations.forEach(function (st) {
-                var item = Lampa.Template.get('card', {title: st.title, card_category: 'UA Radio'});
+                var item = Lampa.Template.get('card', {title: st.title, card_category: 'UA'});
                 item.on('hover:enter', function () {
                     Lampa.Player.play({url: st.url, title: st.title});
                 });
@@ -46,12 +24,8 @@
             });
         }
 
-        var menu_item = $('<div class="menu__item selector" data-action="radio_ua">' +
-            '<div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="6" width="18" height="13" rx="2" stroke="white" stroke-width="2"/><circle cx="12" cy="12.5" r="3.5" stroke="white" stroke-width="2"/><path d="M7 6V4H17V6" stroke="white" stroke-width="2"/></svg></div>' +
-            '<div class="menu__text">Радіо</div>' +
-        '</div>');
-
-        menu_item.on('hover:enter', showRadio);
+        var menu_item = $('<div class="menu__item selector"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 18v-6a9 9 0 0 1 18 0v6"></path><path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3z"></path><path d="M3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z"></path></svg></div><div class="menu__text">Радіо</div></div>');
+        menu_item.on('hover:enter', show);
         $('.menu .menu__list').append(menu_item);
     });
 })();
